fd7ba6eef9ca34ed566a3fae2214be0c86fc5f83
AlwaysasLearner <phyttron6626@gmail.com>
2025-10-28 17:01:52 +0530

    hero section added, with title and image, and small teaser with price and buy button

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef } from 'react'
+
+const Hero = () => {
+  const videoRef = useRef()
+
+  useEffect(() => {
+    if(videoRef.current) videoRef.current.playbackRate = 2;
+  },[])
+  return (
+    <section id='hero'>
+        <div>
+            <h1>Macbook Pro</h1>
+            <img src='/title.png' alt='Macbook Title' />
+            <video src='/videos/hero.mp4' autoPlay muted playsInline ref={videoRef} />
+            <div className='flex-center'>
+                <button>
+                Buy
+            </button>
+            </div>
+            <p>
+                For $1599 or $133/mo for 12 months
+            </p>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
